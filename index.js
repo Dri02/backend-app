635f7ceda3ab22270dc2846f17d068d55fc0c7e4
Dri02 <daliarisla2002@gmail.com>
2025-04-09 10:15:28 -0400

    Update

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const http = require("http");
 const cors = require("cors");
 const express = require("express");
+const morgan = require("morgan");
 const { Server } = require("socket.io");
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use(cors());
 
 const server = http.createServer(app)
 
 const io = new Server(server,{
     cors:{
-        origin: "http://localhost:3000",
-        methods: ["POST"],
+        origin: (origin, callback) => {
+            const allowedOrigins = ["http://localhost:3000", "http://localhost:8000", "http://localhost:8081"];
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("No permitido por CORS"));
+            }
+        },
+        methods: ["GET", "POST"],
     },
 });
 
